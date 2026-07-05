9bf00a769991881b12f278590b82e90a3cff4e5d
Lukas Kurz <lukas@shortdev.de>
2026-07-05 19:11:12 +0200

    feat: IBinaryWritable{T}.MinimumSize

---
+++ ShortDev.IO/Output/IBinaryWritable{T}.cs
@@ -0,0 +1,17 @@
+﻿namespace ShortDev.IO.Output;
+
+public interface IBinaryWritable<TSelf> where TSelf : IBinaryWritable<TSelf>, IBinaryWritable
+{
+    /// <summary>
+    /// Gets the minimum required size in bytes to write this message to a buffer
+    /// </summary>
+    ulong MinimumSize
+    {
+        get
+        {
+            CalcSizeWriter writer = new();
+            ((IBinaryWritable)this).Write(ref writer);
+            return writer.WrittenBinarySize;
+        }
+    }
+}
